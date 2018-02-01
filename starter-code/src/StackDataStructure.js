a268d4a23b73a215a4c71c4f0a08d808de63add7
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-01-31 19:59:13 -0600

    first commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
+
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 0;
+  this.isEmpty = function(){
+   if (this.isEmpty) {
+     return true; 
+   } else {}
+      }
+
+  this.canPush = function (){
+    if (this.canPush) {
+      return true;
+      
+    } else {}
+   
+    }   
+
+  this.push = function (){
+  }
+
+  this.pop = function (){
 }
+
+}//aqui cierra la clase
+
+
